bf7b8dc84a27f5d738c0b7b02ef8239a7a660038
Alexandre Morignot <erdnaxeli@cervoi.se>
2020-06-01 23:12:22 +0200

    !room garbage-collect (not doing anything yet)

--- src/commands/room.cr
+++ src/commands/room.cr
@@ -6,15 +6,13 @@ module Matrix::Architect
   module Commands
     class Room
       enum Order
+        JoinedLocalMembers
+        JoinedMembers
+        Name
         StateEvents
 
         def to_s
-          case self
-          when Order::StateEvents
-            "state_events"
-          else
-            "unknown"
-          end
+          super.underscore
         end
       end
 
@@ -27,14 +25,24 @@ module Matrix::Architect
 
       def self.usage(str)
         str << "\
+
+!room count
+Return the total count of rooms.
+
 !room details ROOM_ID
 Get all details about a room.
+
+!room garbage-collect
+Purge all rooms without any local users joined.
 
 !room top-complexity
 Get top 10 rooms in complexity, aka state events number.
 
 !room top-members
 Get top 10 rooms in number of members.
+
+!room top-local-members
+Get top 10 rooms in number of local members.
 
 !room purge ROOM_ID
 Remove all trace of a room from your database.
@@ -45,12 +53,18 @@ All local users must have left the room before.
       def run(args) : Nil
         command = args.shift?
         case command
+        when "count"
+          count
         when "details"
           details args
+        when "garbage-collect"
+          garbage_collect
         when "top-complexity"
-          complexity
+          top_rooms Order::StateEvents
+        when "top-local-members"
+          top_rooms Order::JoinedLocalMembers
         when "top-members"
-          members
+          top_rooms Order::JoinedMembers
         when "purge"
           purge args
         else
@@ -91,16 +105,15 @@ All local users must have left the room before.
         end
       end
 
-      private def complexity : Nil
+      private def count
         begin
-          rooms = get_rooms Order::StateEvents
+          rooms = get_rooms limit: 0
         rescue ex : Connection::ExecError
           @conn.send_message @room_id, "Error: #{ex.message}"
         else
-          msg = build_rooms_list rooms[0, 10], Order::StateEvents.to_s
-          html = build_rooms_list rooms[0, 10], Order::StateEvents.to_s, is_html: true
-          @conn.send_message @room_id, msg, html
+          @conn.send_message @room_id, "There are #{rooms.size} rooms on this HS"
         end
+
       end
 
       private def details(args) : Nil
@@ -120,19 +133,33 @@ All local users must have left the room before.
         end
       end
 
-      private def get_rooms(order, limit = 0)
+      private def garbage_collect
+        begin
+          rooms = get_rooms Order::JoinedLocalMembers, limit: 0, reverse: true
+        rescue ex : Connection::ExecError
+          @conn.send_message @room_id, "Error: #{ex.message}"
+          return
+        end
+
+        idx = -1
+        rooms.each_index { |i| rooms[i]["joined_local_members"].as_i == 0 && (idx = i)}
+        @conn.send_message @room_id, "Found #{idx + 1} rooms to garbage collect"
+      end
+
+      private def get_rooms(order = Order::Name, limit = 10, reverse = false)
         response = @conn.get "/v1/rooms", is_admin: true, order_by: order
         rooms = response["rooms"].as_a
 
-        while (limit == 0 || rooms.size <= limit) && (next_batch = !response["next_batch"]?)
-          response = @conn.get "/v1/rooms", is_admin: true, order_by: order, from: next_batch
+        while (limit == 0 || rooms.size <= limit) && (next_batch = response["next_batch"]?)
+          if reverse
+            response = @conn.get "/v1/rooms", is_admin: true, order_by: order, from: next_batch, dir: "b"
+          else
+            response = @conn.get "/v1/rooms", is_admin: true, order_by: order, from: next_batch
+          end
           rooms.concat response["rooms"].as_a
         end
 
         return rooms
-      end
-
-      private def members : Nil
       end
 
       private def purge(args)
@@ -148,6 +175,18 @@ All local users must have left the room before.
           @conn.send_message @room_id, "Error: #{ex.message}"
         else
           @conn.send_message @room_id, "#{room_id} purged"
+        end
+      end
+
+      private def top_rooms(order : Order) : Nil
+        begin
+          rooms = get_rooms order
+        rescue ex : Connection::ExecError
+          @conn.send_message @room_id, "Error: #{ex.message}"
+        else
+          msg = build_rooms_list rooms[0, 10], order.to_s
+          html = build_rooms_list rooms[0, 10], order.to_s, is_html: true
+          @conn.send_message @room_id, msg, html
         end
       end
     end
