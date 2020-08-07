26304eb755efea934924f3d77e6d11167959adf1
Alexandre Morignot <erdnaxeli@cervoi.se>
2020-08-07 23:04:14 +0200

    Option to use an alternate config file

--- src/matrix-architect.cr
+++ src/matrix-architect.cr
@@ -1,4 +1,5 @@
 require "log"
+require "option_parser"
 require "yaml"
 
 require "./bot"
@@ -22,18 +23,27 @@ module Matrix::Architect
     property users_id : Array(String)
   end
 
-  def self.get_config
-    File.open("config.yml") do |file|
+  def self.get_config(config_file)
+    File.open(config_file) do |file|
       return Config.from_yaml(file)
     end
   rescue File::NotFoundError
-    puts "Configuration file 'config.yml' not found"
+    puts "Configuration file '#{config_file}' not found"
   rescue ex : YAML::ParseException
     puts "Error while reading config file: #{ex.message}"
   end
 
   def self.run
-    config = get_config
+    config_file = "config.yml"
+    OptionParser.parse do |parser|
+      parser.on("--config CONFIG_FILE", "specify a config file") { |c| config_file = c }
+      parser.on("-h", "--help", "show this help") do
+        puts parser
+        exit
+      end
+    end
+
+    config = get_config(config_file)
     if !config.nil?
       ::Log.setup(config.log_level)
       Bot.new(config).run
